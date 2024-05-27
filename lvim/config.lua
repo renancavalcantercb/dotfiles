5dc05160544f41787f58c127ba9ea7ffb9190782
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-05-27 01:57:50 -0300

    refactor: added gruvbox theme

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,3 +2,12 @@ lvim.leader = ","
 require('user.commands').setup()
 require('user.keys').setup()
 require('user.plugins')
+
+-- Configuração do tema gruvbox
+vim.opt.background = "dark" -- ou "light" dependendo da sua preferência
+vim.g.gruvbox_contrast_dark = "hard"
+
+vim.defer_fn(function()
+  vim.cmd("colorscheme gruvbox")
+end, 100)
+
--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -2,5 +2,10 @@ local M = {}
 
 table.insert(lvim.plugins, {"wakatime/vim-wakatime"})
 table.insert(lvim.plugins, { "github/copilot.vim" })
+table.insert(lvim.plugins, {
+  "ellisonleao/gruvbox.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+})
 
 return M
+
