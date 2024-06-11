1333dbbcf91d12906cdf6865a766ad54b2fbe1b8
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-06-11 17:58:37 -0300

    refactor: changed github copilot key

--- lvim/lua/user/keys.lua
+++ lvim/lua/user/keys.lua
@@ -25,7 +25,7 @@ function M.setup()
     }
 
     vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 end
 
