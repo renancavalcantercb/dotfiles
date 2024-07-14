25c1f55cd6103f13ef2f30821e3f3ecb13eba398
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-07-14 06:26:56 -0300

    feat: added wakatime and github copilot to lazyvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,2 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
