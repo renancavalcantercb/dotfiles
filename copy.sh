9da1c2afc345b82b4f2c2f7d7dd52e88a06fe04d
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-07-14 04:07:15 -0300

    refactor: renamed dotfiles.sh to copy.sh

--- copy.sh
+++ copy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 DEST_DIR="/Users/renan-dev/Desktop/dotfiles"
+NVIM_DIR="${DEST_DIR}/nvim"
 LVIM_DIR="${DEST_DIR}/lvim"
 FISH_DIR="${DEST_DIR}/fish"
 
+mkdir -p "${NVIM_DIR}"
 mkdir -p "${LVIM_DIR}"
 mkdir -p "${FISH_DIR}"
 
@@ -13,7 +15,8 @@ cp ~/.config/lvim/config.lua "${LVIM_DIR}/"
 
 cp -r ~/.config/lvim/lua "${LVIM_DIR}/"
 
+cp -r ~/.config/nvim/ "${NVIM_DIR}/"
+
 cp -r ~/.config/fish/* "${FISH_DIR}/"
 
 echo "Copied dotfiles to ${DEST_DIR}"
-
