a6e9ec56cda3a2cc35eb6c95fcd129af3a93e474
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-08-06 18:25:43 -0300

    feat: added a keymap to copy all content

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,3 @@
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("n", "<C-c>", "ggVGy", { noremap = true, silent = true })
