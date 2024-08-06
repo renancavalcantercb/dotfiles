5dd75fbe1b035cb03457d9bb017b4134071add6b
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-08-06 18:25:51 -0300

    feat: added utf-8 encoding

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- vim.cmd("language en_US")
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8,latin1"
