4b4a1010ab46eeeabbfb3d138afe78fcf6f49ef1
renan <renancavalcantercb@protonmail.com>
2024-10-16 02:23:58 -0300

    feat: added new structure

--- nvim/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("n", "<C-c>", "ggVGy", { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>vs",
-  ":VenvSelect<CR>",
-  { noremap = true, silent = true, desc = "Select VirtualEnv" }
-)
