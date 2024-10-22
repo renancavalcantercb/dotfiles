a6cb8b63c093d3cbd7ece1197d3c89e5234ccb0a
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-10-22 00:20:24 -0300

    feat: added new keymaps for nvim

--- macos/nvim/lua/config/keymaps.lua
+++ macos/nvim/lua/config/keymaps.lua
@@ -1,9 +1,19 @@
 vim.g.copilot_no_tab_map = true
+
 vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
+
 vim.api.nvim_set_keymap("n", "<C-c>", "ggVGy", { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap(
   "n",
   "<leader>vs",
   ":VenvSelect<CR>",
   { noremap = true, silent = true, desc = "Select VirtualEnv" }
 )
+
+-- allow use of `d` to delete without yanking
+vim.api.nvim_set_keymap("n", "d", "_d", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true, silent = true })
+
+-- allow use of `c` to change without yanking
+vim.api.nvim_set_keymap("n", "c", '"_c', { noremap = true, silent = true })
