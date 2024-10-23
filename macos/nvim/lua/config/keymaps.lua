53bea0aa2e9b1c12f8750cb2b158af2a5cc85da2
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-10-22 23:32:17 -0300

    feat: improved the copy buffer lines

--- macos/nvim/lua/config/keymaps.lua
+++ macos/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.copilot_no_tab_map = true
 
 vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
 
-vim.api.nvim_set_keymap("n", "<C-c>", "ggVGy", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-c>", ":w<CR>ggVGy", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap(
   "n",
