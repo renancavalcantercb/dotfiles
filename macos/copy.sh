8ae7126af6c4ff222403702315bad76251a31ac4
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-10-25 03:00:23 -0300

    feat: added hammerspoon and sketchybar dotfiles

--- macos/copy.sh
+++ macos/copy.sh
@@ -4,10 +4,14 @@ DEST_DIR="/Users/renan-dev/Desktop/dotfiles/macos/"
 NVIM_DIR="${DEST_DIR}/nvim"
 LVIM_DIR="${DEST_DIR}/lvim"
 FISH_DIR="${DEST_DIR}/fish"
+HAMMERSPOON_DIR="${DEST_DIR}/.hammerspoon"
+SKETCHYBAR_DIR="${DEST_DIR}/.config/sketchybar"
 
 mkdir -p "${NVIM_DIR}"
 mkdir -p "${LVIM_DIR}"
 mkdir -p "${FISH_DIR}"
+mkdir -p "${HAMMERSPOON_DIR}"
+mkdir -p "${SKETCHYBAR_DIR}"
 
 cp ~/.tmux.conf "${DEST_DIR}/"
 
@@ -18,5 +22,9 @@ cp -r ~/.config/lvim/lua "${LVIM_DIR}/"
 cp -r ~/.config/nvim/ "${NVIM_DIR}/"
 
 cp -r ~/.config/fish/* "${FISH_DIR}/"
+
+cp -r ~/.hammerspoon/ "${HAMMERSPOON_DIR}"
+
+cp -r ~/.config/sketchybar/ "${SKETCHYBAR_DIR}"
 
 echo "Copied dotfiles to ${DEST_DIR}"
