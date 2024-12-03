58b0c841dee3610f2bb5f8d34213cb07b815e5ec
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-12-03 15:10:30 -0300

    feat: add python path

--- macos/nvim/lua/config/options.lua
+++ macos/nvim/lua/config/options.lua
@@ -9,3 +9,6 @@ vim.opt.fileencoding = "utf-8"
 vim.opt.fileencodings = "utf-8,latin1"
 
 vim.opt.relativenumber = false
+
+vim.g.python3_host_prog = "/opt/homebrew/Cellar/python@3.11/3.11.9_1/bin/python3.11"
+vim.env.PYTHONPATH = "/opt/homebrew/lib/python3.11/site-packages"
