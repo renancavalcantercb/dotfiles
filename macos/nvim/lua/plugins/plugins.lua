c5495ea8a2b6c9bdc9103558864697da38240674
Renan Cavalcante <renancavalcantercb@protonmail.com>
2025-05-05 16:03:47 -0300

    fix: python lint on vim

--- macos/nvim/lua/plugins/plugins.lua
+++ macos/nvim/lua/plugins/plugins.lua
@@ -1,3 +1,5 @@
+require("lazyvim.plugins.extras.lsp.none-ls")
+
 return {
   {
     "wakatime/vim-wakatime",
@@ -11,16 +13,6 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "pyright", "black" })
-    end,
-  },
-
-  -- Setup null-ls with `black`
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      vim.list_extend(opts.sources, { nls.builtins.formatting.black })
     end,
   },
   {
