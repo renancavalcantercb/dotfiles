f7d934b7ae19d36c365ebc7df9af1968c49b8d94
Renan Cavalcante <renancavalcantercb@protonmail.com>
2025-05-19 00:59:44 -0300

    fix: mason on new version

--- macos/nvim/lua/plugins/plugins.lua
+++ macos/nvim/lua/plugins/plugins.lua
@@ -1,40 +1,46 @@
 require("lazyvim.plugins.extras.lsp.none-ls")
 
 return {
-  {
-    "wakatime/vim-wakatime",
-  },
-  {
-    "github/copilot.vim",
-  },
-  -- Add `pyright` to mason
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "pyright", "black" })
-    end,
-  },
-  {
-    dir = "/Users/renan-dev/Desktop/estudos/cli/db-cli-nvim",
-    config = function()
-      vim.api.nvim_create_user_command("DBQuery", function(opts)
-        require("dbcli").run_query(opts.args)
-      end, { nargs = 1 })
-    end,
-  },
-  -- todo.nvim local plugin test
-  -- {
-  --   dir = "/Users/renan-dev/Desktop/estudos/lua/todo.nvim",
-  --   event = { "BufReadPre", "BufNewFile" }, -- Use eventos separados
-  --   config = function()
-  --     require("todo").setup()
-  --   end,
-  -- },
-  {
-    "renancavalcantercb/todo.nvim",
-    config = function()
-      require("todo").setup()
-    end,
-  },
+    {
+        "wakatime/vim-wakatime",
+    },
+    {
+        "github/copilot.vim",
+    },
+    {
+        dir = "/Users/renan-dev/Desktop/estudos/cli/db-cli-nvim",
+        config = function()
+            vim.api.nvim_create_user_command("DBQuery", function(opts)
+                require("dbcli").run_query(opts.args)
+            end, { nargs = 1 })
+        end,
+    },
+    -- todo.nvim local plugin test
+    -- {
+    --   dir = "/Users/renan-dev/Desktop/estudos/lua/todo.nvim",
+    --   event = { "BufReadPre", "BufNewFile" }, -- Use eventos separados
+    --   config = function()
+    --     require("todo").setup()
+    --   end,
+    -- },
+    {
+        "renancavalcantercb/todo.nvim",
+        config = function()
+            require("todo").setup()
+        end,
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "shellcheck",
+                "shfmt",
+                "stylua",
+                "prettier",
+                "eslint_d",
+                "python-lsp-server",
+                "gopls",
+            },
+        },
+    },
 }
